c3e987951f0decf2612b68b2f2e5e8d948de74af
ResonaHub <Andhalkark@gmail.com>
2019-09-16 22:18:55 +0530

    SecondCommit

---
+++ src/DailyTask/FindTwoMaxDigit.java
@@ -0,0 +1,16 @@
+package DailyTask;
+
+public class FindTwoMaxDigit {
+	public static void main(String[] args) {
+		int a[] = { 1, 6, 3, 8, 9, 2, 15, 58 };
+		int temp = 0;
+		int i = a.length - 1;
+		for (; i >= 0; i--) {
+			System.out.print(" " + a[i]);
+		}
+		
+		
+
+	}
+
+}
